abad064bb49cf417ad5199c73730b89e40811a7d
Eugene Ng <eugene1862@gmail.com>
2024-06-17 16:23:36 -0700

    added file to record naneye output

---
+++ record_naneye.py
@@ -0,0 +1,5 @@
+# plan is to have files that can record naneye data -> get raw data
+# files that do data processing on the raw data
+# save data to buffer -> pickle dump so we don't have to do aruco processing multiple times
+
+
