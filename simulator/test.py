0a5d28145aaa17cefb886100738a7e98fcfbc5d9
Eugene Ng <eugene1862@gmail.com>
2024-08-02 16:22:13 -0700

    br

--- simulator/test.py
+++ simulator/test.py
@@ -3,9 +3,13 @@ import cv2
 
 folder_name = f"{utils_file.getCurrPath()}/logs/test"
 
-video = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 ret, frame = video.read()
 if not ret:
     print("failed to grab frame")
 
 img_name = f"{folder_name}/images/capture.png"
+print(img_name)
+cv2.imwrite(img_name, frame)
+cv2.imshow("Frame", frame)
+cv2.waitKey(0)
